16de6d41511bfc4d17296a0cf1453198bb573ccc
mohameds277 <mohameds277@gmail.com>
2022-06-03 12:30:54 +0300

    added few lines

--- project3.c
+++ project3.c
@@ -4,34 +4,55 @@
 #include<pthread.h>
 
 
-void* routine()
+void* routine_thread_test()
 {
     printf("Test from threads\n");
     sleep(3);
     printf("ENDING THREAD\n");
 
 }
+
+
+
+
 int main(int argc , char* argv[])
 {
+    pthread_t line1; //thread 1 
+    pthread_t line2; //thread 2
+    pthread_t line3; //thread 3
+    pthread_t line4; //thread 4
+    pthread_t line5; //thread 5 
 
-    pthread_t t1; //thread 1
-    pthread_t t2; //thread 2
-
-    if(pthread_create(&t1 , NULL , &routine , NULL) != 0 ) // intilizing the thread 1 
+    if(pthread_create(&line1 , NULL , &routine_thread_test , NULL) != 0 ) // intilizing the thread 1 
     {
         perror("Failed to create a  thread 1 ");
     } 
 
-
-    if(pthread_create(&t2 , NULL , &routine , NULL) != 0 ) // intilizing the thread 2 
+    if(pthread_create(&line2 , NULL , &routine_thread_test , NULL) != 0 ) // intilizing the thread 2 
+    {
+        perror("Failed to create a  thread 2 ");
+    } 
+     if(pthread_create(&line3 , NULL , &routine_thread_test , NULL) != 0 ) // intilizing the thread 2 
+    {
+        perror("Failed to create a  thread 2 ");
+    } 
+     if(pthread_create(&line4 , NULL , &routine_thread_test , NULL) != 0 ) // intilizing the thread 2 
+    {
+        perror("Failed to create a  thread 2 ");
+    } 
+     if(pthread_create(&line5 , NULL , &routine_thread_test , NULL) != 0 ) // intilizing the thread 2 
     {
         perror("Failed to create a  thread 2 ");
     } 
 
 
-    pthread_join(t1 , NULL); //wait for the thread 
-    pthread_join(t2 , NULL);
-    
+
+    pthread_join(line1 , NULL); //wait for the thread 
+    pthread_join(line2 , NULL);
+    pthread_join(line3 , NULL);
+    pthread_join(line4 , NULL);
+    pthread_join(line5 , NULL);
+
 
 
 
